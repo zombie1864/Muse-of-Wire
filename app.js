147ba287fe5f00dbf1200fdd7a97e98d681129ab
jeff <zombie1864@gmail.com>
2020-08-10 15:59:02 -0400

    app changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
