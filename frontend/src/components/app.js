eda1f1567df016f6949503c8526ad7d47b0f959b
matthewgreer <matthew.greer.dev@gmail.com>
2020-08-10 15:07:30 -0400

    Install dependencies for backend testing

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
-import MainPage from "./main/main_page";
+import MainSplashPage from "./main/main_splash_page";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 
@@ -10,7 +10,7 @@ const App = () => (
   <div>
     <NavBarContainer />
     <Switch>
-      <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/" component={MainSplashPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </Switch>
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
-import session from './session_reducer';
-// import errors from './errors_reducer';
+import session from './session_api_reducer';
+import errors from './errors_reducer';
 
 
 const RootReducer = combineReducers({
   session,
-  // errors
+  errors
 });
 
 export default RootReducer;
