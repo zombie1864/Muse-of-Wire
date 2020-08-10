3b36a5b133d6b121478c356ea899e71ba1a5d834
matthewgreer <matthew.greer.dev@gmail.com>
2020-08-10 10:52:43 -0400

    Add comments to index, re: use of session token

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,13 @@ import { logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
+
+  // check if user has session token in localStorage
+
   if (localStorage.jwtToken) {
+    // if so, set session token in header of all axios requests
     setAuthToken(localStorage.jwtToken);
+    // decode user's info from token
     const decodedUser = jwt_decode(localStorage.jwtToken);
 
     const preloadedState = {
@@ -23,14 +28,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const currentTime = Date.now() / 1000;
 
+    // check whether user has an expired token
     if (decodedUser.exp < currentTime) {
+      // if token has expired, logout & redirect to login page
       store.dispatch(logout());
       window.location.href = '/login';
     }
   } else {
+    // if user has no token configure empty store
     store = configureStore({});
   }
   const root = document.getElementById('root');
-
   ReactDOM.render(<Root store={store} />, root);
 });
