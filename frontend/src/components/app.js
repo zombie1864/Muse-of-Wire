4c94d2072df95284eb8db20f45d6deaaafe00259
matthewgreer <matthew.greer.dev@gmail.com>
2020-08-11 10:09:20 -0400

    Remove video container refs from app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 import MainSplashPage from "./main/main_splash_page";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
-import ShowVideoContainer from './videos/show_video_container'; 
-import IndexVideoContainer from './videos/video_index_container'; 
+// import ShowVideoContainer from './videos/show_video_container'; 
+// import IndexVideoContainer from './videos/video_index_container'; 
 
 const App = () => (
   <div>
     <NavBarContainer />
     <Switch>
-      < Route path = '/api/videos/:id' component={ShowVideoContainer}/>
+      {/* < Route path = '/api/videos/:id' component={ShowVideoContainer}/> */}
       <AuthRoute exact path="/" component={MainSplashPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
