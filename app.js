f63807bb156c1224d928e05cb06f7d8be51f4819
jeff <zombie1864@gmail.com>
2020-08-12 10:15:41 -0400

    changes to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const path = require('path'); 
 
 require('dotenv').config();
 
@@ -14,7 +15,12 @@ mongoose
 .catch((err) => console.log(err));
 
 
-app.get("/", (req, res) => res.send("Welcome to Muse of Wire!!"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.use(passport.initialize());
 require("./config/passport")(passport);
