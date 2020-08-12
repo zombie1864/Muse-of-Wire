7bb53c3337448194e32462bb8245111bb905c94c
jeff <zombie1864@gmail.com>
2020-08-12 09:18:13 -0400

    undo changes

--- app.js
+++ app.js
@@ -24,5 +24,5 @@ app.use(bodyParser.json());
 app.use("/api/users", users);
 
 // for testing
-const port = process.env.PORT; // || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
