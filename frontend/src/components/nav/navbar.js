010d8f912b24a3cc9251a98799b2b8c0824654c9
matthewgreer <matthew.greer.dev@gmail.com>
2020-08-12 22:24:08 -0400

    Add :hover & :active behavior to MW logo in Navbar

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -78,7 +78,14 @@ class NavBar extends React.Component {
           <div className="navigation-container">
             <Link to={"/"}>
               <div className="m-w-logo-container">
-                <img src="https://lh3.googleusercontent.com/h_StglhPQkLAAgon_Viq-bN1J26k0mHjS5r30e2Ofl9okpkq3NRFyFGmckPRH187t1JBqiMybYV8li8BJH5XScW_ELjx7foDGAUJ-TX48r-lyycaEXlCvy_e26JMqZfKjg_DmpLslHUmF0s3LHzyttoYElAUE6c27nx6ISQUdKnQOv_6koUVtYOQA1ToUWgz2HYoPK90TwUveOQsfkjBjBaaJvhHOeXF3YD5FkfAlU_9AgLMYSxT72uv4r7Krbg-5jPmF3Smp_hDZaLO9upfLwqB3-s5Ill6v1ol7FiVtrWyrWDWBMPdfUBvQNaewD3Jc4GbKDpVdPNlKEOs6R6NT-ep0s2iOeZGzNdVjDxpjOsoH36MxYnrzdPKiES4AMarCrTVBvNCqAc_HKr86EM3QbldzLChf1JqLVY4_HIsqmPLWX-cH0LnJwfSFnwD-xgnEOHQzh31t6L4G2KnDire4nC0l3l56xN9_OVKxmRfOG1vp_u0d6v21H0azcJKQnoYGlHHYDJZDvUr6pes3Hav0IqLjz9XsA1c1NG7PuTE-8dttni7yb5Gdz4pAP2wtnJFqusIFT_9FkHOjkV-QTGjIG9iZZwhamshi2VMbXfiRQu4Kzr8_7H47tuV9LUWwPcI_arKq-63HYuAxC2LoUdgq-GLtVOqr6sXUBa9l-hBSgC0i-fVm6dLL7_d5cJpiG3YR9dV2A=w3298-h1808-ft" />
+                <img
+                  src="https://lh3.google.com/u/0/d/1CeO7JEqkDVQpUzyxAlTcquAc059ERD_j=w1616-h1808-iv1"
+                  className="m-w-logo-image"
+                />
+                <img 
+                  src="https://lh3.google.com/u/0/d/10xrgro8JEo5VD24pYRHf2xuA5zLDqDMM=w3564-h1808-iv1"
+                  className="m-w-logo-image-hover" 
+                />
               </div>
             </Link>
             {this.getLinks()}
