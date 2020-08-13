b5a423d1fd3eb3d997b7643f19cee6475d743910
matthewgreer <matthew.greer.dev@gmail.com>
2020-08-13 14:06:10 -0400

    Create sidebar of images. Needs work

--- frontend/src/components/main/main_splash_page.js
+++ frontend/src/components/main/main_splash_page.js
@@ -9,8 +9,8 @@ class MainSplashPage extends React.Component {
         <div className="body-container splash-background-image">
           <div className="splash-muse-of-wire-logo-container">
             <img
-              className="muse-of-wire-logo" 
-              src="https://lh3.google.com/u/0/d/1fPkEaNX-8lq4ZTFKtzbKyEJbWLsSbzxq=w3208-h1808-iv1" 
+              className="muse-of-wire-logo"
+              src="https://lh3.googleusercontent.com/L4HLdfILkS8WL1rokMGgCgifkkA37b8sYIT98gEz3lH5O5ReVTUF-izTtbMnc4kudQDWigqIFRYdKwqLUW2xbC3MJah9LAlcCO00coFLjROLkmcnwWBpnvWcoq9tSezKV4h5_9YK_OBso4U9IXZ7G9eHpJ9fazezSb26-dlO5DS9B0UX9DSGQHXxg5lkN6IdfdAmjsHcAm-JSeprKGlj-G3eIW9FsJYkdf1BVUp7uJ3a-2el9GOs4zm4aCF1CCezB2wsm91PBUebSLrsmiuYDWawHZeRI8wd0AqabborOyJMolbG3EFQoPe91dGbekA00YDsXnMXnSkEhbf3J52AllcL-sVHQW3ARD2ArOkw2VR2LKISxVgW5nMe68iiBD4sfA_HPL62IKoIDVEUqJpPRMYGw7PraWXjE37y0lpWvKWKp-9Fh7eexI5YZNWdPSM77fuROFoPq38kQXklZl9dnmCupRcF_f_0e1yeZr2OeW2dKVDbATJc_bP58xd5mkiHrymQp-by6ib_6T1Rdy3c0QToHuOZnzvRDCnp_10rZ1kjVtM-OSraa_-YBgvkJY8ZbFkKCyzLfAnviAYytDjQl-ztTZz-f-w3hOk4f1K2Fg6Q_3zLvIXJi6f3fCF6FCipw6DiPMCAn1WlMxwsc6Ty2zF2vOwOr82fNmWacux9sJ02mehAHZ9LUxTjfNjGlBbTJ8sw1w=w3232-h1808-ft"
             />
           </div>
           <div className="splash-image-container">
--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -79,12 +79,12 @@ class NavBar extends React.Component {
             <Link to={"/"}>
               <div className="m-w-logo-container">
                 <img
-                  src="https://lh3.google.com/u/0/d/1CeO7JEqkDVQpUzyxAlTcquAc059ERD_j=w1616-h1808-iv1"
+                  src="https://lh3.googleusercontent.com/CpSLsZPxX4uxfKQYf7X2rh9hD338-TFCwJtgfD99yIRpcG3x-Fei8AVD4qPqnHsbEL7B1Ffgcwx7mZA5jUApOA0RyWOhR9rzHh5VY1FFl_hl5fx4oHhw2-pC08Fmrbt68of1-qiwCZqjx4W3cU67SSuW7nMW7JD2oSBNe1LiCinJv37UF2dNvAdBPnqZKHmIUkBRe-8V30EcBi0CgBiEmSVP805fTP84iraGj0rsb_C8XagCMeYNG2QBFWT1jX670TCRG5BHK-_rdAZ7RcBWL4CTUBjLJuHqOmBe17QrFlKu2rSeiq_KPFNth3g0CjSHVArkP9BAeATvfbOTD0tWitEU-OTNx-6zAejjRylfOpeQ_hqAOzOkjLCb4-rCEh_SxpzozNLMgYsMcTfyfCuw0jfnZi61FAxUq9QU_WhRczl3MoWensaR3jy90PKs-9hdgWwqaNi_avqiLDKRKkOj2hBx6ZrFtaY_L2sJm6ETi1pgGOh-3W8F8s5mryZNbQd0_cKhZDqSoCQVuoUggFKj2fRIZkrdvNlsi6P92hHOlsrvV2Apztc8VOvYLOj5PtRfvi0SzXOf16nATfh82wR-dhIAajPbLBHC7fu1k6fbgjYRoeq16VVDHDq4uo23gkpLhTauNifuh-C55JfE6JKf_M7_qp86MLOdgZk-bGY7rlHFGVyjhF0fpy0qxqS7ESsRm3hMHA=w3232-h1808-ft"
                   className="m-w-logo-image"
                 />
-                <img 
-                  src="https://lh3.google.com/u/0/d/10xrgro8JEo5VD24pYRHf2xuA5zLDqDMM=w3564-h1808-iv1"
-                  className="m-w-logo-image-hover" 
+                <img
+                  src="https://lh3.googleusercontent.com/KHPWCQ76hqkibj5Eg4mSmVhnxJUcZERKfblCe3PD2s2fu_nlDBf8vvIhKeQIUr592aQ5QbSIyEOhQ08XsVIsXpq4O5KibZIYX-jyjnbDFnBdZkrqlo_Lnjp9A4Rd0X8k_eV9_9w4x6qokAy60mHXsskjDEjt0Ni7Eh3dWUB3T8rwkM0lgUbwHFMaYHBC7c0hj4nidIfGIVsk9GhhlLhkyFznrXVrkUq8udiLA40-IllmkfkSYszqYBJa6Ai1Q5OyND8pXWzsWzi8P22t3i-o72unUy7NUEN9dS1Ssj1nGdnh9KbVGjPvOBTG6Cym5eJ5JVvHgiUavP5cOPztQzAfk3Eh4Z0CudT0PZiCd_JxiTl1WVkZfk4HB-ZCvOTWN50kSLOb9X7npaM6FaTLrlDINy-JAoJIOYlzt6qXNhh8ikf3KWm1AHAryiMXGOrYxDAyVW4VaZJ5r-W2bz82u0Iui1Afcq_17tDPoxJYgq7uv9vNa7GfJH9ux33694EI88-rWEXSIwwoVLnuHzif0FrC1lC7n3J74gvAnLJP3936_3Qb94o0heGllaoCTJCy60gRaD7qPx6s76C8BeFng-2BoblaWGOrGrR_Mx_LNh-gce25QDMWhCtjwscTqaIEJWU6c72kgo9LmFZgJuluPIySN2LHadDxO1jliTwgwFzLRnw80JQUiN_fPCK-u2mtxogs1F8ong=w1660-h1808-ft"
+                  className="m-w-logo-image-hover"
                 />
               </div>
             </Link>
