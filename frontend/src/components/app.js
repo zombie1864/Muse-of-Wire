517ab33c2aa8faac3d7bfd4c661c8c6c5087d034
matthewgreer <matthew.greer.dev@gmail.com>
2020-08-13 17:15:21 -0400

    Add title in head element, protect creators route

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -16,7 +16,7 @@ const App = () => (
     <NavBarContainer />
  
     {/* < Route path = '/api/videos/:id' component={ShowVideoContainer}/> */}
-    <Route exact path = '/creators' component = {CreatorPage} />
+    <ProtectedRoute exact path = '/creators' component = {CreatorPage} />
     <AuthRoute exact path="/" component={MainSplashPage} />
     <AuthRoute exact path="/login" component={LoginFormContainer} />
     <AuthRoute exact path="/signup" component={SignupFormContainer} />
