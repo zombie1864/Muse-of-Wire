fb8fc03703889324904ba1a1588cc2dd3412b225
Robert Kornblum <robkornblum44@gmail.com>
2020-08-31 14:59:31 -0400

    update search backend

--- frontend/src/util/search_api_util.js
+++ frontend/src/util/search_api_util.js
@@ -5,7 +5,7 @@ export const apiSearchImages = (imageQuery) => {
   return axios.post('/api/images/search', imageQuery);
 };
 export const apiSearchVideos = (videoQuery) => {
-  return axios.get('/api/videos/search', videoQuery);
+  return axios.post('/api/videos/search', videoQuery);
 };
 // export const searchUsers = (userQuery) => {
 //   return axios.get('/api/users', userQuery);
